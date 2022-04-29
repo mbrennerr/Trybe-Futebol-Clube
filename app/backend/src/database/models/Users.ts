fb5a7edc3df13ef6699afb4694613e1d8998ff60
mbrennerr <marciobrennerbusiness@gmail.com>
2022-04-29 16:16:49 -0300

    Feat: create_Users_Model

---
+++ app/backend/src/database/models/Users.ts
@@ -0,0 +1,49 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+import { IUsers } from '../../Types';
+
+export default class Users extends Model implements IUsers {
+  public id: number;
+
+  public username: string;
+
+  public role: string;
+
+  public email: string;
+
+  public password: string;
+}
+
+Users.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: false,
+      autoIncrement: true,
+    },
+    username: {
+      type: STRING(20),
+      allowNull: false,
+    },
+    role: {
+      type: STRING(30),
+      allowNull: false,
+    },
+    email: {
+      type: STRING(50),
+      allowNull: false,
+    },
+    password: {
+      type: STRING(30),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'users',
+    underscored: true,
+    timestamps: false,
+
+  },
+);
