d0a3b0b6e47ab1706f615303cccca3dd106401d6
mbrennerr <marciobrennerbusiness@gmail.com>
2022-05-05 17:21:28 -0300

    Feat: add_login_controller

---
+++ app/backend/src/controller/LoginController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { ILogin } from '../Types';
+import LoginService from '../services/LoginService';
+
+export default class LoginController {
+  public loginService ;
+
+  constructor() {
+    this.loginService = new LoginService();
+  }
+
+  public getByEmail = async (req:Request, res:Response) => {
+    const { email, password }:ILogin = req.body;
+    const user = await this.loginService.findUserByEmail(email, password);
+    return res.status(201).json(user);
+  };
+}
