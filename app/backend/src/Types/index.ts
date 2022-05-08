5dd4ca7cfc20eb07b5089437ca8b9bcd25c0df53
mbrennerr <marciobrennerbusiness@gmail.com>
2022-05-08 19:43:41 -0300

    Feat:add_Error_interface

--- app/backend/src/Types/index.ts
+++ app/backend/src/Types/index.ts
@@ -1,9 +1,13 @@
 import ITeams from './ITeams';
 import IMatches from './IMatches';
 import IUsers from './IUsers';
+import IErrorMap from './IErrorMap';
+import ILogin from './ILogin';
 
 export {
   ITeams,
   IMatches,
   IUsers,
+  IErrorMap,
+  ILogin,
 };
