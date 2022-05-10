62d178fdf3f55a00cbd688db0ca4253ab91dfce3
mbrennerr <marciobrennerbusiness@gmail.com>
2022-05-10 14:34:47 -0300

    Feat: add_generate_token_to_service

--- app/backend/src/services/LoginService.ts
+++ app/backend/src/services/LoginService.ts
@@ -1,6 +1,9 @@
 import Users from '../database/models/Users';
+import Token from '../Utils';
 
 export default class LoginService {
+  private token = Token.Token;
+
   private usersModel;
 
   constructor() {
@@ -9,15 +12,20 @@ export default class LoginService {
 
   public async findUserByEmail(email:string, _pass:string) {
     const user = await this.usersModel.findOne({ where: { email } });
+    if (!user) {
+      throw new Error('Incorrect email or password');
+    }
     const result = {
       user: {
-        id: user?.id,
-        username: user?.username,
-        role: user?.role,
-        email: user?.email,
-
+        id: user.id,
+        username: user.username,
+        role: user.role,
+        email: user.email,
       },
+
     };
-    return result;
+
+    const token = this.token.generateToken(result.user);
+    return { user: result.user, token };
   }
 }
