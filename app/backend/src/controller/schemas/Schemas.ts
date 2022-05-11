5ce78cdd1c8ef67b3a2ee0673ce81a29e7e7c686
mbrennerr <marciobrennerbusiness@gmail.com>
2022-05-10 22:15:22 -0300

    Chore:add_Joi_messages_Params

--- app/backend/src/controller/schemas/Schemas.ts
+++ app/backend/src/controller/schemas/Schemas.ts
@@ -1,19 +1,25 @@
 import * as Joi from 'joi';
+import { EmailMsg, PswMsg } from '../../Utils/Messages';
 
 export default class Schemas {
   public static get Login() {
     return Joi.object({
       email: Joi
-        .string()
-        .email()
-        .not()
-        .empty()
-        .required(),
+        .string().email().not().empty()
+        .required()
+        .messages({
+          'any.required': EmailMsg.requiredField,
+          'string.email': EmailMsg.incorrectEmail,
+          'string.base': EmailMsg.incorrectEmail,
+          // 'string.min':
+        }),
       password: Joi
-        .string()
-        .not()
-        .empty()
-        .required(),
+        .string().not().empty().required()
+        .messages({
+          'any.required': PswMsg.requiredField,
+          'string.base': PswMsg.incorrectPsw,
+
+        }),
     });
   }
 }
