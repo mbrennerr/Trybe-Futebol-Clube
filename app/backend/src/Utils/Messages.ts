a2d0aa2ead661cc94882f976e5b7204254bf8faa
mbrennerr <marciobrennerbusiness@gmail.com>
2022-05-10 22:14:15 -0300

    Feat: Create_errors_messages

---
+++ app/backend/src/Utils/Messages.ts
@@ -0,0 +1,12 @@
+enum EmailMsg{
+  requiredField = 'All fields must be filled',
+  incorrectEmail = 'Incorrect email or password',
+}
+enum PswMsg{
+  requiredField = 'All fields must be filled',
+  incorrectPsw = 'Incorrect email or password',
+}
+
+export { EmailMsg,
+  PswMsg,
+};
