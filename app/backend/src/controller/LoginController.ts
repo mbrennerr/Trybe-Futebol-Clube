d052809b6196a19034145995e626f646509bf83e
mbrennerr <marciobrennerbusiness@gmail.com>
2022-05-15 22:35:24 -0300

    Refactor: add_returnRole_and_try_cat

--- app/backend/src/controller/LoginController.ts
+++ app/backend/src/controller/LoginController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { ILogin } from '../Types';
 import LoginService from '../services/LoginService';
 
@@ -9,9 +9,20 @@ export default class LoginController {
     this.loginService = new LoginService();
   }
 
-  public getByEmail = async (req:Request, res:Response) => {
-    const { email, password }:ILogin = req.body;
-    const user = await this.loginService.findUserByEmail(email, password);
-    return res.status(200).json(user);
+  public getByEmail = async (req:Request, res:Response, next:NextFunction) => {
+    try {
+      const { email, password }:ILogin = req.body;
+      const user = await this.loginService.findUserByEmail(email, password);
+      return res.status(200).json(user);
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  public returnUserRole = async (req:Request, res:Response, _next:NextFunction) => {
+    console.log('role');
+    console.log('meu Locals', res.locals);
+    const { role } = res.locals.user;
+    return res.status(200).json(role);
   };
 }
