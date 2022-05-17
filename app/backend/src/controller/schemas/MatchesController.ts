a1af3a43c47bd4f3af1d05fe564bf3e1bee7ba79
mbrennerr <marciobrennerbusiness@gmail.com>
2022-05-17 18:33:46 -0300

    Feat:Create_getAllMatches_Controller_method

---
+++ app/backend/src/controller/schemas/MatchesController.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import MatchesService from '../../services/MatchesService';
+
+export default class MatchesController {
+  private matchesService;
+
+  constructor() {
+    this.matchesService = new MatchesService();
+  }
+
+  public getAllMatches = async (_req:Request, res:Response, next:NextFunction) => {
+    try {
+      const matches = await this.matchesService.findAllMatches();
+      return res.status(200).json(matches);
+    } catch (error) {
+      return next(error);
+    }
+  };
+}
