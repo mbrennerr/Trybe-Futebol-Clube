770273dc408f31ca25f34bc4178ca2af0e4bb040
mbrennerr <marciobrennerbusiness@gmail.com>
2022-05-17 22:05:59 -0300

    Feat: add_getByQuery_methods_controller

--- app/backend/src/controller/MatchesController.ts
+++ app/backend/src/controller/MatchesController.ts
@@ -8,10 +8,21 @@ export default class MatchesController {
     this.matchesService = new MatchesService();
   }
 
-  public getAllMatches = async (_req:Request, res:Response, next:NextFunction) => {
+  public getAllMatches = async (req:Request, res:Response, next:NextFunction) => {
     try {
       const matches = await this.matchesService.findAllMatches();
-      return res.status(200).json(matches);
+      const { inProgress } = req.query;
+      console.log('log do query', inProgress);
+      if (!inProgress) { return res.status(200).json(matches); }
+
+      if (inProgress === 'true') {
+        const isTrue = await this.matchesService.findByQuery(true);
+        return res.status(200).json(isTrue);
+      }
+      if (inProgress === 'false') {
+        const isFalse = await this.matchesService.findByQuery(false);
+        return res.status(200).json(isFalse);
+      } throw new Error(`${inProgress} must be ONLY true or false`);
     } catch (error) {
       return next(error);
     }
