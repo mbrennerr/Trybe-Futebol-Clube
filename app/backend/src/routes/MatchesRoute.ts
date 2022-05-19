f8b7bc76c76839f48950b61cefa219bccc5d8968
mbrennerr <marciobrennerbusiness@gmail.com>
2022-05-18 21:46:36 -0300

    Feat:add_post_match_route;

--- app/backend/src/routes/MatchesRoute.ts
+++ app/backend/src/routes/MatchesRoute.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import Validate from '../middlewares/Validate';
 import MatchesController from '../controller/MatchesController';
 
 const router:Router = Router();
 const controller = new MatchesController();
 
 router.get('/matches', controller.getAllMatches);
+router.post('/matches', Validate.Token, controller.postMatch);
 export default router;
