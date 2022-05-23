c47978c463c26132552b7a93ccb5150ad20de879
mbrennerr <marciobrennerbusiness@gmail.com>
2022-05-23 15:17:54 -0300

    Feat:add_login_email_password_test_cases;

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -14,12 +14,33 @@ const { expect } = chai;
 describe('Login Tests', () => {
   describe('Email Validation', () => {
     let chaiHttpResponse: Response;
-    it('Should be a String', async () => {
+    it('👥 Should be a String', async () => {
          chaiHttpResponse = await chai
-         .request(app).post('/login').send({email:123654879}) 
-         expect(chaiHttpResponse.status).to.be.eq(422);
-         expect(chaiHttpResponse.body.error).to.be.eq("\"email\" must be a string")
+         .request(app).post('/login').send({email:123145652}) 
+         expect(chaiHttpResponse.status).to.be.eq(401);
+         expect(chaiHttpResponse.body.message).to.be.eq('Incorrect email or password')
        });
+    it('Must not be empty', async () =>{
+      chaiHttpResponse =  await chai
+      .request(app)
+      .post('/login').send({email: ''})
+      expect(chaiHttpResponse.status).to.be.eq(400);
+      expect(chaiHttpResponse.body.message).to.be.eq('All fields must be filled')
+    });
+    it('Must have a valid format', async  () => {
+      chaiHttpResponse = await chai
+      .request(app)
+      .post('/login').send({email:'xablaus'})
+      expect(chaiHttpResponse.status).to.eq(401)
+      expect(chaiHttpResponse.body.message).to.be.eq('Incorrect email or password')
+    });
+    it('Email is required', async  () => {
+      chaiHttpResponse = await chai
+      .request(app)
+      .post('/login').send({password:'xablaus'})
+      expect(chaiHttpResponse.status).to.eq(400)
+      expect(chaiHttpResponse.body.message).to.be.eq('All fields must be filled')
+    })
 
   })
 })
