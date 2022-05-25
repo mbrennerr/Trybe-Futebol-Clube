117db3b198c3736ba4560f68263835a3794c0aa1
mbrennerr <marciobrennerbusiness@gmail.com>
2022-05-24 23:52:20 -0300

    Feat:add_leaderBoardGeneralRoute

--- app/backend/src/routes/LeaderBoardRoute.ts
+++ app/backend/src/routes/LeaderBoardRoute.ts
@@ -6,4 +6,5 @@ const controller = new LeaderBoardController();
 
 router.get('/leaderboard/home', controller.getLeaderboardHome);
 router.get('/leaderboard/away', controller.getLeaderBoardAway);
+router.get('/leaderboard', controller.getLeaderBoardGeneral);
 export default router;
